14bac57bff4dbbd4f327306a47dc045c4424075a
N-Hristov <nicolahristov93@gmail.com>
2014-07-27 19:13:47 +0300

    Updated code with collision detection

--- Garbage Collector OOP/gamescriptOOP.js
+++ Garbage Collector OOP/gamescriptOOP.js
@@ -74,10 +74,16 @@ function Drawable() {
     this.speed = 0;
     this.canvasWidth = 0;
     this.canvasHeight = 0;
+    this.collidableWith = "";
+    this.isColliding = false;
+    this.type = "";
     // Define abstract function to be implemented in child objects
     this.draw = function() {
     };
     this.move = function() {
+    };
+    this.isCollidableWith = function(object) {
+        return (this.collidableWith === object.type);
     };
 }
 
@@ -131,6 +137,9 @@ function Bullet(object) {
     this.draw = function() {
         this.context.clearRect(this.x-1, this.y-1, this.width+1, this.height+1);
         this.y -= this.speed;
+        if (this.isColliding) {
+            return true;
+        }
         if (self === "bullet" && this.y <= 0 - this.height) {
             return true;
         }
@@ -155,9 +164,181 @@ function Bullet(object) {
         this.y = 0;
         this.speed = 0;
         this.alive = false;
+        this.isColliding = false;
     };
 }
 Bullet.prototype = new Drawable();
+
+/**
+ * QuadTree object.
+ *
+ * The quadrant indexes are numbered as below:
+ *     |
+ *  1  |  0
+ * —-+—-
+ *  2  |  3
+ *     |
+ */
+function QuadTree(boundBox, lvl) {
+    var maxObjects = 10;
+    this.bounds = boundBox || {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0
+    };
+    var objects = [];
+    this.nodes = [];
+    var level = lvl || 0;
+    var maxLevels = 5;
+    /*
+     * Clears the quadTree and all nodes of objects
+     */
+    this.clear = function() {
+        objects = [];
+        for (var i = 0; i < this.nodes.length; i++) {
+            this.nodes[i].clear();
+        }
+        this.nodes = [];
+    };
+    /*
+     * Get all objects in the quadTree
+     */
+    this.getAllObjects = function(returnedObjects) {
+        for (var i = 0; i < this.nodes.length; i++) {
+            this.nodes[i].getAllObjects(returnedObjects);
+        }
+        for (var i = 0, len = objects.length; i < len; i++) {
+            returnedObjects.push(objects[i]);
+        }
+        return returnedObjects;
+    };
+    /*
+     * Return all objects that the object could collide with
+     */
+    this.findObjects = function(returnedObjects, obj) {
+        if (typeof obj === "undefined") {
+            console.log("UNDEFINED OBJECT");
+            return;
+        }
+        var index = this.getIndex(obj);
+        if (index != -1 && this.nodes.length) {
+            this.nodes[index].findObjects(returnedObjects, obj);
+        }
+        for (var i = 0, len = objects.length; i < len; i++) {
+            returnedObjects.push(objects[i]);
+        }
+        return returnedObjects;
+    };
+    /*
+     * Insert the object into the quadTree. If the tree
+     * excedes the capacity, it will split and add all
+     * objects to their corresponding nodes.
+     */
+    this.insert = function(obj) {
+        if (typeof obj === "undefined") {
+            return;
+        }
+        if (obj instanceof Array) {
+            for (var i = 0, len = obj.length; i < len; i++) {
+                this.insert(obj[i]);
+            }
+            return;
+        }
+        if (this.nodes.length) {
+            var index = this.getIndex(obj);
+            // Only add the object to a subnode if it can fit completely
+            // within one
+            if (index != -1) {
+                this.nodes[index].insert(obj);
+                return;
+            }
+        }
+        objects.push(obj);
+        // Prevent infinite splitting
+        if (objects.length > maxObjects && level < maxLevels) {
+            if (this.nodes[0] == null) {
+                this.split();
+            }
+            var i = 0;
+            while (i < objects.length) {
+                var index = this.getIndex(objects[i]);
+                if (index != -1) {
+                    this.nodes[index].insert((objects.splice(i,1))[0]);
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+    };
+    /*
+     * Determine which node the object belongs to. -1 means
+     * object cannot completely fit within a node and is part
+     * of the current node
+     */
+    this.getIndex = function(obj) {
+        var index = -1;
+        var verticalMidpoint = this.bounds.x + this.bounds.width / 2;
+        var horizontalMidpoint = this.bounds.y + this.bounds.height / 2;
+        // Object can fit completely within the top quadrant
+        var topQuadrant = (obj.y < horizontalMidpoint && obj.y + obj.height < horizontalMidpoint);
+        // Object can fit completely within the bottom quandrant
+        var bottomQuadrant = (obj.y > horizontalMidpoint);
+        // Object can fit completely within the left quadrants
+        if (obj.x < verticalMidpoint &&
+            obj.x + obj.width < verticalMidpoint) {
+            if (topQuadrant) {
+                index = 1;
+            }
+            else if (bottomQuadrant) {
+                index = 2;
+            }
+        }
+        // Object can fix completely within the right quandrants
+        else if (obj.x > verticalMidpoint) {
+            if (topQuadrant) {
+                index = 0;
+            }
+            else if (bottomQuadrant) {
+                index = 3;
+            }
+        }
+        return index;
+    };
+    /*
+     * Splits the node into 4 subnodes
+     */
+    this.split = function() {
+        // Bitwise or [html5rocks]
+        var subWidth = (this.bounds.width / 2) | 0;
+        var subHeight = (this.bounds.height / 2) | 0;
+        this.nodes[0] = new QuadTree({
+            x: this.bounds.x + subWidth,
+            y: this.bounds.y,
+            width: subWidth,
+            height: subHeight
+        }, level+1);
+        this.nodes[1] = new QuadTree({
+            x: this.bounds.x,
+            y: this.bounds.y,
+            width: subWidth,
+            height: subHeight
+        }, level+1);
+        this.nodes[2] = new QuadTree({
+            x: this.bounds.x,
+            y: this.bounds.y + subHeight,
+            width: subWidth,
+            height: subHeight
+        }, level+1);
+        this.nodes[3] = new QuadTree({
+            x: this.bounds.x + subWidth,
+            y: this.bounds.y + subHeight,
+            width: subWidth,
+            height: subHeight
+        }, level+1);
+    };
+}
 
 /**
  * Custom Pool object. Holds Bullet objects to be managed to prevent
@@ -176,6 +357,8 @@ function Pool(maxSize) {
                 // Initalize the object
                 var bullet = new Bullet("bullet");
                 bullet.init(0,0, imageRepository.bullet.width, imageRepository.bullet.height);
+                bullet.collidableWith = "enemy";
+                bullet.type = "bullet";
                 pool[i] = bullet;
             }
         }
@@ -190,9 +373,22 @@ function Pool(maxSize) {
             for (var i = 0; i < size; i++) {
                 var bullet = new Bullet("enemyBullet");
                 bullet.init(0,0, imageRepository.enemyBullet.width, imageRepository.enemyBullet.height);
+                bullet.collidableWith = "ship";
+                bullet.type = "enemyBullet";
                 pool[i] = bullet;
             }
         }
+    };
+    // Function for the QuadTree collision detection to get objects in pool
+    // and give them to it for checking as an array
+    this.getPool = function() {
+        var obj = [];
+        for (var i = 0; i < size; i++) {
+            if (pool[i].alive) {
+                obj.push(pool[i]);
+            }
+        }
+        return obj;
     };
     /*
      * Grabs the last item in the list and initializes it and
@@ -235,52 +431,6 @@ function Pool(maxSize) {
     };
 }
 
-// The keycodes that will be mapped when a user presses a button.
-// Original code by Doug McInnes
-KEY_CODES = {
-    32: 'space',
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-};
-// Creates the array to hold the KEY_CODES and sets all their values
-// to false. Checking true/false is the quickest way to check status
-// of a key press and which one was pressed when determining
-// when to move and which direction.
-KEY_STATUS = {};
-for (code in KEY_CODES) {
-    KEY_STATUS[ KEY_CODES[ code ]] = false;
-}
-/**
- * Sets up the document to listen to onkeydown events (fired when
- * any key on the keyboard is pressed down). When a key is pressed,
- * it sets the appropriate direction to true to let us know which
- * key it was.
- */
-document.onkeydown = function(e) {
-    // Firefox and opera use charCode instead of keyCode to
-    // return which key was pressed.
-    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-    if (KEY_CODES[keyCode]) {
-        e.preventDefault();
-        KEY_STATUS[KEY_CODES[keyCode]] = true;
-    }
-};
-/**
- * Sets up the document to listen to ownkeyup events (fired when
- * any key on the keyboard is released). When a key is released,
- * it sets the appropriate direction to false to let us know which
- * key it was.
- */
-document.onkeyup = function(e) {
-    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-    if (KEY_CODES[keyCode]) {
-        e.preventDefault();
-        KEY_STATUS[KEY_CODES[keyCode]] = false;
-    }
-};
-
 /**
  * Create the Ship object that the player controls. The ship is
  * drawn on the "ship" canvas and uses dirty rectangles to move
@@ -291,8 +441,10 @@ function Ship() {
     this.speed = 3;
     this.bulletPool = new Pool(30);
     this.bulletPool.init("bullet");
-    var fireRate = 15;
+    var fireRate = 9;
     var counter = 0;
+    this.collidableWith = "enemyBullet";
+    this.type = "ship";
     this.draw = function() {
         this.context.drawImage(imageRepository.spaceship, this.x, this.y);
     };
@@ -305,8 +457,7 @@ function Ship() {
             // be redrawn in it's new location
             this.context.clearRect(this.x, this.y, this.width, this.height);
             // Update x and y according to the direction to move and
-            // redraw the ship. Change the else if's to if statements
-            // to have diagonal movement.
+            // redraw the ship.
             if (KEY_STATUS.left) {
                 this.x -= this.speed;
                 if (this.x <= 0) { // Keep player within the screen
@@ -332,12 +483,15 @@ function Ship() {
                 }
             }
             // Finish by redrawing the ship
-            this.draw();
+            if (!this.isColliding) {
+                this.draw();
+            }
+          }
+           if (KEY_STATUS.space && counter >= fireRate && !this.isColliding) {
+             this.fire();
+              counter = 0;
         }
-        if (KEY_STATUS.space && counter >= fireRate) {
-            this.fire();
-            counter = 0;
-        }
+
     };
     /*
      * Fires two bullets
@@ -358,6 +512,8 @@ function Enemy() {
     var percentFire = .01;
     var chance = 0;
     this.alive = false;
+    this.collidableWith = "bullet";
+    this.type = "enemy";
     /*
      * Sets the Enemy values
      */
@@ -391,11 +547,17 @@ function Enemy() {
             this.y -= 5;
             this.speedX = -this.speed;
         }
-        this.context.drawImage(imageRepository.enemy, this.x, this.y);
-        // Enemy has a chance to shoot every movement
-        chance = Math.floor(Math.random()*101);
-        if (chance/100 < percentFire) {
-            this.fire();
+        if (!this.isColliding) {
+            this.context.drawImage(imageRepository.enemy, this.x, this.y);
+            // Enemy has a chance to shoot every movement
+            chance = Math.floor(Math.random()*101);
+            if (chance/100 < percentFire) {
+                this.fire();
+            }
+            return false;
+        }
+        else {
+            return true;
         }
     };
     /*
@@ -414,6 +576,7 @@ function Enemy() {
         this.speedX = 0;
         this.speedY = 0;
         this.alive = false;
+        this.isColliding = false;
     };
 }
 Enemy.prototype = new Drawable();
@@ -484,6 +647,8 @@ function Game() {
             }
             this.enemyBulletPool = new Pool(50);
             this.enemyBulletPool.init("enemyBullet");
+            // Start QuadTree
+            this.quadTree = new QuadTree({x:0,y:0,width:this.mainCanvas.width,height:this.mainCanvas.height});
             return true;
         } else {
             return false;
@@ -504,6 +669,16 @@ function Game() {
  * object.
  */
 function animate() {
+    // Insert objects into quadtree
+    game.quadTree.clear();
+    game.quadTree.insert(game.ship);
+    game.quadTree.insert(game.ship.bulletPool.getPool());
+    game.quadTree.insert(game.enemyPool.getPool());
+    game.quadTree.insert(game.enemyBulletPool.getPool());
+
+    detectCollision();
+
+    // Animate game objects
     requestAnimFrame( animate );
     game.background.draw();
     game.ship.move();
@@ -511,6 +686,72 @@ function animate() {
     game.enemyPool.animate();
     game.enemyBulletPool.animate();
 }
+
+// Collision Detection
+function detectCollision() {
+    var objects = [];
+    game.quadTree.getAllObjects(objects);
+    for (var x = 0, len = objects.length; x < len; x++) {
+        game.quadTree.findObjects(obj = [], objects[x]);
+        for (y = 0, length = obj.length; y < length; y++) {
+            // DETECT COLLISION ALGORITHM
+            if ( objects[x].collidableWith === obj[y].type &&
+                (objects[x].x < obj[y].x + obj[y].width &&
+                    objects[x].x + objects[x].width  > obj[y].x &&
+                    objects[x].y < obj[y].y + obj[y].height &&
+                    objects[x].y + objects[x].height > obj[y].y)) {
+                objects[x].isColliding = true;
+                obj[y].isColliding = true;
+            }
+        }
+    }
+}
+
+// The keycodes that will be mapped when a user presses a button.
+// Original code by Doug McInnes
+KEY_CODES = {
+    32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
+// Creates the array to hold the KEY_CODES and sets all their values
+// to false. Checking true/false is the quickest way to check status
+// of a key press and which one was pressed when determining
+// when to move and which direction.
+KEY_STATUS = {};
+for (code in KEY_CODES) {
+    KEY_STATUS[ KEY_CODES[ code ]] = false;
+}
+/**
+ * Sets up the document to listen to onkeydown events (fired when
+ * any key on the keyboard is pressed down). When a key is pressed,
+ * it sets the appropriate direction to true to let us know which
+ * key it was.
+ */
+document.onkeydown = function(e) {
+    // Firefox and opera use charCode instead of keyCode to
+    // return which key was pressed.
+    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+    if (KEY_CODES[keyCode]) {
+        e.preventDefault();
+        KEY_STATUS[KEY_CODES[keyCode]] = true;
+    }
+};
+/**
+ * Sets up the document to listen to ownkeyup events (fired when
+ * any key on the keyboard is released). When a key is released,
+ * it sets the appropriate direction to false to let us know which
+ * key it was.
+ */
+document.onkeyup = function(e) {
+    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+    if (KEY_CODES[keyCode]) {
+        e.preventDefault();
+        KEY_STATUS[KEY_CODES[keyCode]] = false;
+    }
+};
 
 /**
  * requestAnim shim layer by Paul Irish
